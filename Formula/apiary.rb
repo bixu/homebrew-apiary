a7beaa1d475c6f1d3abbef8460a164866ba52007
Blake Irvin <blakeirvin@me.com>
2025-11-21 15:45:38 +0100

    Get rid of hipster `v`

--- Formula/apiary.rb
+++ Formula/apiary.rb
@@ -7,18 +7,18 @@ class Apiary < Formula
 
   on_macos do
     on_intel do
-      url "https://github.com/bixu/apiary/releases/download/v#{version}/apiary-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/bixu/apiary/releases/download/#{version}/apiary-x86_64-apple-darwin.tar.gz"
       sha256 '257e1492605921fc388397550a307e07d012a68d04b779b52f74cf790135a505'
     end
 
     on_arm do
-      url "https://github.com/bixu/apiary/releases/download/v#{version}/apiary-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/bixu/apiary/releases/download/#{version}/apiary-aarch64-apple-darwin.tar.gz"
       sha256 '55b67b77c062d81445869e00c6a5d71b5449488ba0dd0968ecc866be9d90295f'
     end
   end
 
   on_linux do
-    url "https://github.com/bixu/apiary/releases/download/v#{version}/apiary-x86_64-unknown-linux-gnu.tar.gz"
+    url "https://github.com/bixu/apiary/releases/download/#{version}/apiary-x86_64-unknown-linux-gnu.tar.gz"
     sha256 'a2bcec7bb31c347c8fbf44b08b923c462914a367ee887dfb042c809e04e813e9'
   end
 
