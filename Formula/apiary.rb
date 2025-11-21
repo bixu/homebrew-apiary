b999854975dae644cc2d69aa949591eff749b54c
Blake Irvin <blakeirvin@me.com>
2025-11-21 15:32:58 +0100

    creat false shas for testing

--- Formula/apiary.rb
+++ Formula/apiary.rb
@@ -8,12 +8,12 @@ class Apiary < Formula
   on_macos do
     on_intel do
       url "https://github.com/bixu/apiary/releases/download/v#{version}/apiary-x86_64-apple-darwin.tar.gz"
-      sha256 '348a8cfbcf369e781d7cad55a1d664ae6eb4ac9448891fe06e6c473d388de6f0'
+      sha256 'foo'
     end
 
     on_arm do
       url "https://github.com/bixu/apiary/releases/download/v#{version}/apiary-aarch64-apple-darwin.tar.gz"
-      sha256 'ee7d57f7f70ad9c239b5dddbcfb6afbce739f41733803324548095634edd47ef'
+      sha256 'bar'
     end
   end
 
