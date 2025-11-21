d7997d1feb7124e8f66588dfe6fa0850ee490d8e
Blake Irvin <blakeirvin@me.com>
2025-11-21 13:11:25 +0100

    Refactor Apiary formula for version and architecture

--- Formula/apiary.rb
+++ Formula/apiary.rb
@@ -1,35 +1,28 @@
+# Homebrew Formula for Apiary CLI
 class Apiary < Formula
-  # Update these values as needed as new versions are released
-  current_version = "0.2.0"
-  current_darwin_arm_sha256 = ""
-  current_darwin_intel_sha256 = "5a7f175bc86c82e4c1f6abb6382cb9b6210d7cc036b3ac65120822f1f8f2bb68"
+  desc 'Comprehensive CLI for Honeycomb API with dual authentication'
+  homepage 'https://github.com/bixu/apiary'
+  version '0.1.0'
+  license 'MIT'
 
   on_macos do
-    @source_os = "darwin"
-    if Hardware::CPU.arm?
-      @source_sha256 = current_darwin_arm_sha256
-      @source_arch = "aarch64"
-    elsif Hardware::CPU.intel?
-      @source_sha256 = current_darwin_intel_sha256
-      @source_arch = "x86_64"
+    on_intel do
+      url "https://github.com/bixu/apiary/releases/download/v#{version}/apiary-x86_64-apple-darwin.tar.gz"
+      sha256 'PLACEHOLDER_INTEL_SHA256'
+    end
+
+    on_arm do
+      url "https://github.com/bixu/apiary/releases/download/v#{version}/apiary-aarch64-apple-darwin.tar.gz"
+      sha256 'PLACEHOLDER_ARM_SHA256'
     end
   end
-  on_linux do
-    raise "linux not supported"
-  end
-  source_url = "https://github.com/bixu/apiary/releases/download/#{current_version}/apiary_#{current_version}_#{@source_arch}-apple-#{@source_os}.zip"
-
-  desc "A command-line interface to the Honeycomb API"
-  homepage "https://github.com/bixu/apiary"
-  url source_url
-  version current_version
-  sha256 @source_sha256
 
   def install
-    bin.install "apiary"
+    bin.install 'apiary'
   end
 
   test do
-    `#{bin}/apiary --version`
+    system "#{bin}/apiary", '--help'
+    assert_match 'A comprehensive CLI for Honeycomb API', shell_output("#{bin}/apiary --help")
   end
 end
